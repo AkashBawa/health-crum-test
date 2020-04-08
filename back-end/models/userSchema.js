c7e4d93dea1f5860652234e72746d4ae5239ec8c
AkashBawa <bawa2027@gmail.com>
2020-04-09 00:15:15 +0530

    schema for user profile

---
+++ back-end/models/userSchema.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+//var quest = mongoose.model('questions');
+var Schema = mongoose.Schema;
+
+var sch = new Schema({
+   
+    name : {type : String, required : true},
+    //uestionId : { type: Schema.Types.ObjectId, ref: 'questions'}
+    
+    hraQuestions : [
+        {
+            questionId : { type: Schema.Types.ObjectId, ref: 'questions'},
+            ans : {type : String},
+            score : {type : Number},                                 // store 1, 2,3, 4 as user points
+            subans : [{
+                ans : {type : String},
+                score : {type : Number}
+            }]
+        }
+    ]
+})
+
+module.exports = mongoose.model('user', sch);
