e76b4bd4a4aed8004739e1a18eb5e5cfafca2edc
AkashBawa <bawa2027@gmail.com>
2020-04-10 01:19:49 +0530

    final api to store

--- back-end/controllers/userInput.js
+++ back-end/controllers/userInput.js
@@ -7,11 +7,10 @@ const Schema = mongoose.Schema;
 
 // from UI we need to get main question _id then its subquestion _id
 
-var userId = "5e8e24aece22d82f60e2af28"    // in real time this id is replaced by the id of user login
+var userId = "5e8efa895b324a3e4c97a278"   // in real time this id is replaced by the id of user login
 
 exports.saveUserAns =  async function(req, res) {
-    
-    
+
     var fromUser = [{
         questionId :"5e8c2cb0e3ddc11f5058a607",           // main question _id
         mainans : "yes",
@@ -22,24 +21,67 @@ exports.saveUserAns =  async function(req, res) {
         }]
     }]
     
-  
-    var userProf = new userProfile();
-    var user = new userProfile({
-        name : "akash"
-    })
-  
-    /*await user.save((err, result)=>{
-        if(err) console.log(err)
-        else console.log(result)
-    });*/
+    var hrSave = {
+        questionId : fromUser[0].questionId,
+        //questionId : result._id,
+        ans : fromUser[0].mainans,
+        score : fromUser[0].mainScore,
+        subans: []
+    }
+    var subans = [];
+   
+    for(var j = 0 ; j < fromUser[0].subquestion.length; j++) {
+        var subans = {
+            ans : fromUser[0].subquestion[j].subAns,
+            score : fromUser[0].subquestion[j].value
+        }
+        hrSave.subans.push(subans);
+    }
+    console.log(hrSave);
 
+    console.log("before")
+    var activeUser = await userProfile.find({_id : userId})
+    console.log("Active user" , activeUser)
+    // first time user true hra
+    if(activeUser.length > 0) {
+        console.log("active user");
+        
+        let present= await userProfile.find({_id : '5e8f6a9cd548ec412c1b332b', 'hraQuestions.questionId' : fromUser[0].questionId})
+        
+        if(!present) {
+            console.log("first attempt")    
+            userProfile.update({name : "akash"},{ $push : {hraQuestions : hrSave}} , function(err, update){
+                if (err) console.log(err);
+                else console.log("new question added");
+            })
+            
+        }
+        // user attempt this question second time 
+        else{
+            console.log("else reach")
+            console.log(hrSave);
+            userProfile.update({_id : '5e8f6a9cd548ec412c1b332b', 'hraQuestions.questionId' : fromUser[0].questionId}, {$set :{hraQuestions : hrSave}}, (err,resul)=>{
+                console.log(resul)
+            })
+        }
+    }
+    else {
+        console.log("new user");
+        var userProf = new userProfile();
+        var user = new userProfile({
+            name : "bawa",
+            hraQuestions : hrSave 
+        })
+        await user.save();
+    }
+
+    /*
     questions.findOne({}, async function(err, result) {
        if(err){
            console.log(err);
        }
        else{
           // console.log(result);
-
            for(let i = 0; i < fromUser.length; i++) {
              var hrSAve = {
                  //questionId : fromUser[i].questionId,
@@ -60,7 +102,7 @@ exports.saveUserAns =  async function(req, res) {
              var del = {
                  ans : "Akash project"
              }
-             await userProfile.update({name : "akash"},  { $set: {hraQuestions :  { questionId : result._id}}},function(err, result) {
+             await userProfile.update({_id :"5e8efa895b324a3e4c97a278"},  { $set: {hraQuestions :  { questionId : fromUser.questionId}}},function(err, result) {
                  if(err) {
                      console.log(err);
                  }
@@ -85,23 +127,6 @@ exports.saveUserAns =  async function(req, res) {
                  
             });
        }
-        userProfile.findOne({name : "akash"}, function(err, result){
-            if(err) console.log(err);
-            else console.log("last" + result)
-            })
-   })
-/*
-        }  
-    })
-    /*
-    userProfile.findOne({}, function(err, result){
-        if(err)
-            console.log(err);
-        else{
-            console.log(result)
-        }
-    })
-
-    */
+   })*/
     res.send("saved ")
 }
